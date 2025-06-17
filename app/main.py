68cf9538b40d802aefbafd4f64d1d678ba0b1fde
Nicolás Aranguren Turmequé <nicolasarangurenturmeque@Laptop-de-Nicolas.local>
2025-06-16 20:12:11 -0500

    push out to ubuntu

--- app/main.py
+++ app/main.py
@@ -27,4 +27,4 @@ app.include_router(auth.router)
 @app.get("/")
 def root():
     # return {"message": "Successfully deployed from CI/CD pipeline !"}
-    return {"message": "Pushing out to Ubuntu !"}
+    return {"message": "Pushing out to Ubuntu 😀"}
